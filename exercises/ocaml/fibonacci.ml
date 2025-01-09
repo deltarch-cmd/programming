5e0ec343c9acbf45de3f214bfe2f70daee535a9e
Deluwuta <albertotrejoats149@gmail.com>
2025-01-09 17:44:03 +0100

    Idk

--- exercises/ocaml/fibonacci.ml
+++ exercises/ocaml/fibonacci.ml
@@ -1,10 +1,6 @@
-let rec factorial n =
-    if n = 0 then 1
-    else n * factorial (n - 1)
-
 let rec fibonacci n =
-    if n = 0 || n = 1 then 1 
+    if n = 0 then 1
+    else if n = 1 then 1
     else fibonacci (n - 1) + fibonacci (n - 2)
 
-let () = print_endline(string_of_int (factorial 5))
-let () = print_endline(string_of_int (fibonacci 5))
+let () = print_endline(string_of_int(fibonacci 5))
