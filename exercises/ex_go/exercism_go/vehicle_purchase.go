e78b9ca8ab7ea3a21f93429799d8f2475e327f9b
Deluwuta <albertotrejoats149@gmail.com>
2025-06-30 13:07:48 +0200

    Two Go exercises more

---
+++ exercises/ex_go/exercism_go/vehicle_purchase.go
@@ -0,0 +1,30 @@
+package main
+
+var licensedVehicles = map[string]struct{} {
+	"car": {},
+	"truck": {},
+}
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	_, ok := licensedVehicles[kind]
+	return ok
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	var decision string = option2
+	if option1 < option2 { decision = option1 }
+	return decision + " is clearly the better choice."
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 { 
+		return originalPrice * 0.8 
+	} else if age >= 10 { 
+		return originalPrice * 0.5 
+	} else { 
+		return originalPrice * 0.7 
+	}
+}
